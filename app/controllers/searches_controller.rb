6f82f22c5c060f8d6af8eee1e85323c664b33739
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-22 16:58:43 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -18,6 +18,9 @@ end
  else
    @error = body["meta"]["errorDetail"]
  end
+ rescue Faraday::ConnectionFailed
+   @error = "There was a timeout. Please try again."
+ end
 render 'search'
   end
 end
